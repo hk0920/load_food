20168b09809b9a92daa33e6808218d9dbdb788bf
hk0920 <gmlrb920@naver.com>
2022-12-03 17:52:21 +0900

    api connent data file upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,11 @@ import { useEffect } from "react";
 export function Home(){
     useEffect(()=>{
         var key = "6c6e554874676d6c38324646794978";
-        axios({
-            url:"/api"+key+"/json/LOCALDATA_072405/1/5",
-            withCredentials:true,
-        }).then(function(res){
+        axios.get("http://openapi.seoul.go.kr:8088/" + key + "/json/LOCALDATA_072405/1/5/").then((res)=>{
             console.log(res)
-        }).catch(function(error){
-            console.log("error=>" + error)
-        });
+        }).catch((error)=>{
+            console.log(error)
+        })
     }, [])
 
     return(
